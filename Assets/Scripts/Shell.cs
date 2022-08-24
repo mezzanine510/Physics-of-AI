7561f926548bba84eae7a547c8da21d9e6290aa6
Mezz <mezzanine510@gmail.com>
2022-08-24 11:14:20 -0700

    added custom gravity forces to calculate shell/bullet movement speeds, drag, etc

--- Assets/Scripts/Shell.cs
+++ Assets/Scripts/Shell.cs
@@ -1,35 +1,39 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Shell : MonoBehaviour
 {
     public GameObject explosion;
-    public float speed = 3.0f;
-    float mass = 10f;
-    float force = 1000f;
-    float acceleration;
+    private float speed = 0f;
+    private float ySpeed = 0f;
+    private float mass = 1f;
+    private float force = 4f;
+    private float drag = 1f;
+    private float gravity = -9.8f;
+    private float gravityAcceleration;
+    private float acceleration;
 
     void Start()
     {
-
+        acceleration = force / mass;
+        speed += acceleration * 1f;
+        gravityAcceleration = gravity / mass;
     }
 
     void LateUpdate()
     {
-        acceleration = force / mass;
-        speed += acceleration * Time.deltaTime;
-        this.transform.Translate(0, 0, Time.deltaTime * speed);
+        speed *= (1 - Time.deltaTime * drag);
+        ySpeed += gravityAcceleration * Time.deltaTime;
+        this.transform.Translate(0, Time.deltaTime * ySpeed, Time.deltaTime * speed);
     }
 
     void OnCollisionEnter(Collision col)
     {
-        if (col.gameObject.tag == "tank")
-        {
-            GameObject exp = Instantiate(explosion, this.transform.position, Quaternion.identity);
-            Destroy(exp, 0.5f);
-            Destroy(this.gameObject);
-        }
+        if (col.gameObject.tag != "tank") return;
+        if (col.gameObject.tag != "ground") return;
+
+        GameObject exp = Instantiate(explosion, this.transform.position, Quaternion.identity);
+        Destroy(exp, 0.5f);
+        Destroy(this.gameObject);
     }
 
 }
