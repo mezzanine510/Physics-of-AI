b6ca21c681b8f35ac4d915145db274c39cb28820
Mezz <mezzanine510@gmail.com>
2022-09-04 15:20:31 -0700

    AI tank turret correctly angles itself to aim at target

--- Assets/Scripts/FireShell.cs
+++ Assets/Scripts/FireShell.cs
@@ -5,6 +5,8 @@ public class FireShell : MonoBehaviour
     [SerializeField] GameObject bullet;
     [SerializeField] GameObject turret;
     [SerializeField] GameObject enemy;
+    [SerializeField] Transform turretBase;
+    private float speed = 15f;
     private float rotSpeed = 2f;
 
     void Update()
@@ -12,10 +14,21 @@ public class FireShell : MonoBehaviour
         Vector3 direction = (enemy.transform.position - this.transform.position).normalized;
         Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
         this.transform.rotation = Quaternion.Slerp(this.transform.rotation, lookRotation, Time.deltaTime * rotSpeed);
-        
+
+        RotateTurret();
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             CreateBullet();
+        }
+    }
+
+    void RotateTurret()
+    {
+        float? angle = CalculateAngle(true);
+        if (angle != null)
+        {
+            turretBase.localEulerAngles = new Vector3(360f - (float)angle, 0f, 0f);
         }
     }
 
@@ -24,4 +37,25 @@ public class FireShell : MonoBehaviour
         Instantiate(bullet, turret.transform.position, turret.transform.rotation);
     }
 
+    float? CalculateAngle(bool low)
+    {
+        Vector3 targetDir = enemy.transform.position - this.transform.position;
+        float y = targetDir.y;
+        targetDir.y = 0;
+        float x = targetDir.magnitude;
+        float gravity = 9.8f;
+        float speedSqr = speed * speed;
+        float underTheSqrRoot = (speedSqr * speedSqr) - gravity * (gravity * x * x + 2 * y * speedSqr);
+
+        if (underTheSqrRoot >= 0f)
+        {
+            float root = Mathf.Sqrt(underTheSqrRoot);
+            float highAngle = speedSqr + root;
+            float lowAngle = speedSqr - root;
+            
+            if(low) return (Mathf.Atan2(lowAngle,gravity * x) * Mathf.Rad2Deg);
+            else return (Mathf.Atan2(highAngle, gravity * x) * Mathf.Rad2Deg);
+        }
+        else return null;
+    }
 }
